c2ab42758d7522f64659bde96653aa1357411355
Daniel Burgos <dburgos.developer@gmail.com>
2024-05-11 12:11:01 -0500

    commit nodemon

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,19 @@
 const express = require("express")
-const router = require("./config/Routes/routes.config")
-const {swaggerUi, specs} = require("./swagger")
+require('dotenv').config();
+// const router = require("./config/Routes/routes.config")
+// const {swaggerUi, specs} = require("./swagger")
+const SERVER_PORT = process.env.SERVER_PORT
+
+console.log(SERVER_PORT);
 
 const app = express();
-require('./config/db.config')
+// require('./config/db.config')
 
 app.use(express.json());
-app.use(router)
+// app.use(router)
 
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
+// app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
 
-app.listen(8000, () => {
-    console.log("Listening in port 8000");
+app.listen(SERVER_PORT, () => {
+    console.log("Listening in port " + SERVER_PORT);
 })
