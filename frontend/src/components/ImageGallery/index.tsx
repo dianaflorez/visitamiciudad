82de11010dc2922f5d4570a73d80517fb1a8d7f5
dianaflorez <dianaflorezbravo@gmail.com>
2024-10-23 18:41:36 -0500

    [Add] pages ciudad y cafe

---
+++ frontend/src/components/ImageGallery/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './ImageGallery.css'; // Archivo CSS para los estilos de la galería
+
+type ImageGalleryProps = {
+  images: string[]; // Array de URLs de imágenes
+};
+
+const ImageGallery: React.FC<ImageGalleryProps> = ({ images }) => {
+  return (
+    <div className="gallery-grid">
+      {images.slice(0, 9).map((image, index) => (
+        <div key={index} className="image-container">
+          <img src={image} alt={`Image ${index + 1}`} className="gallery-image" />
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default ImageGallery;
